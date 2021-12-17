387bf9e8c8fb61ba06815cc9394e27e058615dc7
Chris Kennedy <kennedy.christopherian@gmail.com>
2021-12-17 09:33:27 -0600

    updated app.js to return proper pages/components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 // import logo from './logo.svg';
 import React from "react";
 import Album from './pages/Album'
-import AppBar from './components/Appbar'
+// import AppBar from './components/Appbar'
 // import './App.css';
+// import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import { BrowserRouter as Router, Route } from "react-router-dom";
 function App() {
   return (
-    <Router>
-      <div>
-        <Route exact path={[ "/album" ]} component={AppBar} />
-        <Route exact path="/album" component={Album} />
-          {/* <Route path="/details/:_id" component={Details} /> */}
-         {/* <Footer /> */}
-      </div>
-     </Router>
+    <main>
+      {/* <AppBar /> */}
+      <Album />
+    </main>
+    // <Router>
+    //   <div>
+    //     <Route exact path={[ "/album" ]} component={AppBar} />
+    //     <Route exact path="/album" component={Album} />
+    //       {/* <Route path="/details/:_id" component={Details} /> */}
+    //      {/* <Footer /> */}
+    //   </div>
+    //  </Router>
   );
 }
 
